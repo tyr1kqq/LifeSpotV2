39939858d23d6f00df724e464f55737bb4a7a2d3
Igor Turkin <igorturkinwhite@yandex.ru>
2024-05-26 12:59:49 +0200

    bag

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -13,3 +13,19 @@ else {
     window.location.href = "http://www.google.com"
 }
 
+function filterContent() {
+    let elements = document.getElementsByClassName('video-container');
+
+    for (let i = 0; i <= elements.length; i++) {
+        let videoText = elements[i].querySelector(".video-title").innerText;
+        if (!videoText.toLowerCase().includes(inputParseFunction().toLowerCase()))
+        {
+            elements[i].style.display = 'none';
+        }
+        else
+        {
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
+
