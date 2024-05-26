ccd47505dbd2e6429118759e10039657eaa55a08
Igor Turkin <igorturkinwhite@yandex.ru>
2024-05-26 11:42:12 +0200

    Have problem

--- LifeSpot/Startup.cs
+++ LifeSpot/Startup.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Text;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -9,37 +10,49 @@ namespace LifeSpot
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
 
             app.UseRouting();
+
+            // Загружаем отдельные элементы для вставки в шаблон: боковое меню и футер
+            string footerHtml = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Views", "Shared", "footer.html"));
+            string sideBarHtml = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Views", "Shared", "sideBar.html"));
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    // Путь до страницы нашего сайта
                     var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "index.html");
-                    // Загружаем страницу сайта
-                    var html = await File.ReadAllTextAsync(viewPath);
-                    // Сервер возвращает ответ
-                    await context.Response.WriteAsync(html);
+
+                    // Загружаем шаблон страницы, вставляя в него элементы
+                    var html = new StringBuilder(await File.ReadAllTextAsync(viewPath))
+                        .Replace("<!--SIDEBAR-->", sideBarHtml)
+                        .Replace("<!--FOOTER-->", footerHtml);
+
+                    await context.Response.WriteAsync(html.ToString());
                 });
-                
+
+                endpoints.MapGet("/testing", async context =>
+                {
+                    var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "testing.html");
+
+                    // Загружаем шаблон страницы, вставляя в него элементы
+                    var html = new StringBuilder(await File.ReadAllTextAsync(viewPath))
+                        .Replace("<!--SIDEBAR-->", sideBarHtml)
+                        .Replace("<!--FOOTER-->", footerHtml);
+
+                    await context.Response.WriteAsync(html.ToString());
+                });
+
                 endpoints.MapGet("/Static/CSS/index.css", async context =>
                 {
-                    // по аналогии со страницей Index, настроим на нашем сервере путь до страницы со стилями, чтобы браузер знал, откуда их загружать
                     var cssPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "CSS", "index.css");
                     var css = await File.ReadAllTextAsync(cssPath);
                     await context.Response.WriteAsync(css);
@@ -47,12 +60,10 @@ namespace LifeSpot
 
                 endpoints.MapGet("/Static/JS/index.js", async context =>
                 {
-                    // Для JS настроим всё так же, как уже сделали для CSS выше.
                     var jsPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", "index.js");
                     var js = await File.ReadAllTextAsync(jsPath);
                     await context.Response.WriteAsync(js);
                 });
-
             });
         }
     }
