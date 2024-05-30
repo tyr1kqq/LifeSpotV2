fe89f208e55f28808999cadf303bd8c9edf7f7d9
Igor Turkin <igorturkinwhite@yandex.ru>
2024-05-30 11:58:44 +0200

    error

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,16 +1,37 @@
-// Запросим возраст пользователя и сохраним в переменную
+/*
+* Сессия теперь создается в общей области видимости.
+* Будет "захватываться" тремя функциями
+* 
+* */
+let session = new Map();
 
-let age = prompt('Пожалуйста, введите ваш возраст');
-
-if (age >= 18) {
-    // Те, кто старше 18, увидят приветствие и будут направлены на сайт
-    alert("Приветствуем на LifeSpot! " + new Date().toLocaleString());
+let sessionLog = function logSession() {
+    for (let result of session) {
+        console.log(result)
+    }
 }
-else {
-    // Выполним проверку. Если введенное число < 18, либо если введено не число -
-    // пользователь будет перенаправлен
-    alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-    window.location.href = "http://www.google.com"
+
+// Сохранение данных сессии сразу при заходе пользователя на страницу
+function handleSession() {
+    // Сохраним время начала сессии
+    session.set("startDate", new Date().toLocaleString())
+    // Сохраним UserAgent
+    session.set("userAgent", window.navigator.userAgent)
+}
+
+
+// Проверка возраста пользователя
+
+function checkAge() {
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+
+    if (session.get("age") >= 18) {
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
 }
 
 function filterContent() {
@@ -18,14 +39,22 @@ function filterContent() {
 
     for (let i = 0; i <= elements.length; i++) {
         let videoText = elements[i].querySelector(".video-title").innerText;
-        if (!videoText.toLowerCase().includes(inputParseFunction().toLowerCase()))
-        {
+        if (!videoText.toLowerCase().includes(inputParseFunction() /* Захват переменной теперь происходит с помощью замыкания */.toLowerCase())) {
             elements[i].style.display = 'none';
-        }
-        else
-        {
+        } else {
             elements[i].style.display = 'inline-block';
         }
     }
+}
+
+let sessionLog = function logSession(session) {
+    for (let result of session) {
+        console.log(result)
+    }
+
+}
+
+function inputParseFunction() {  
+        return document.getElementsByTagName('input')[0].value.toLowerCase();
 }
 
